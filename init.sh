2b5c1c150c5c064e1c90f11e662862488b982120
Maxime Renou <maximerenou50@gmail.com>
2019-04-07 13:12:11 +0200

    Add .gitconfig file

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@
 [[ ! -s ${HOME}/.bashrc ]] && ln -s ${HOME}/src/maximerenou50/mac-profile/.bashrc ${HOME}/.bashrc
 [[ ! -s ${HOME}/.bash_profile ]] && ln -s ${HOME}/src/maximerenou50/mac-profile/.bash_profile ${HOME}/.bash_profile
 [[ ! -s ${HOME}/.bash_alias ]] && ln -s ${HOME}/src/maximerenou50/mac-profile/.bash_alias ${HOME}/.bash_alias
+[[ ! -s ${HOME}/.gitconfig ]] && ln -s ${HOME}/src/maximerenou50/mac-profile/.gitconfig ${HOME}/.gitconfig
